b7249899d96c7dc240ab0fa28f570c48de2c7fea
aos <aos@kkg-do.eu>
2020-10-26 11:53:07 +0100

    Programmversion 2020.1.1 - Kommentare angepasst, so dass klarer wird, mit welchem Index man auf die jeweilige CD zugreift.

--- src/control/CDCollectionHandler.java
+++ src/control/CDCollectionHandler.java
@@ -12,6 +12,7 @@ public class CDCollectionHandler {
     /**
      * Die Anzahl an Platzgrößen gibt die Anzahl an CD-Ständern vor - hier 4.
      * Die CD-Ständer an sich sind so groß wie die jeweilige Platzgröße.
+     *
      * @param amounts - Platzgrößen der einzelnen CD-Ständer.
      */
     public CDCollectionHandler(int[] amounts){
@@ -20,9 +21,10 @@ public class CDCollectionHandler {
     }
 
     /**
+     * Diese Methode dient dazu, eine neue CD zu instanziieren und sie an die vorgebene Stelle zu packen, sofern dort ein Platz frei ist.
      *
-     * @param box - Gewählter CD-Ständer
-     * @param place - Gewählter Platz
+     * @param box - Index des gewählten CD-Ständers
+     * @param place - Index des gewählten Platzes
      * @param artist - Künstername/Bandname
      * @param title - Albumtitel
      * @return - true, falls ein Platz frei war und die CD hinzugefügt werden konnte, sonst false.
@@ -34,8 +36,9 @@ public class CDCollectionHandler {
 
     /**
      * Diese Methode dient dazu, die Daten einer bestimmten Position im zweidimensionalem Array auszugeben.
-     * @param box - Gewählter CD-Ständer
-     * @param place - Gewählter Platz
+     *
+     * @param box - Index des gewählten CD-Ständers
+     * @param place - Index des gewählten Platzes
      * @return - Entweder ein String-Array mit "Künstler" - "Titel" oder mit "Empty" - "Empty".
      */
     public String[] getInfo(int box, int place){
@@ -46,8 +49,9 @@ public class CDCollectionHandler {
 
     /**
      * Diese Methode dient dem Entfernen einer CD.
-     * @param box - Gewählter CD-Ständer
-     * @param place - Gewählter Platz
+     *
+     * @param box - Index des gewählten CD-Ständers
+     * @param place - Index des gewählten Platzes
      * @return - true, falls eine vorhandene CD entfernt wurde, false, falls keine Cd zum entfernen vorhanden war.
      */
     public boolean releaseCD(int box, int place){
@@ -57,17 +61,19 @@ public class CDCollectionHandler {
 
     /**
      * Diese Methode dient dazu, die enthaltenen Daten aufzubereiten und als String-Array auszugeben.
-     * @param index - CD-Ständer, um den es sich handelt.
+     *
+     * @param box - CD-Ständer, um den es sich handelt.
      * @return Ein Array, das abwechselnd den jeweiligen Künstler und den jeweiligen Albumtitel enthält. Leere Plätze werden mit "Empty" gefüllt.
      */
-    public String[] getAllCDsFrom(int index){
+    public String[] getAllCDsFrom(int box){
         //TODO: 05 - Vollständige Informationsausgabe aller CDs - Nach Fertigstellung im MainPanelHandler Zeile 165-167 entkommentieren
         return null;
     }
 
     /**
      * Diese Methode dient dazu, einen CD-Ständer zu komprimieren. Dabei rücken spätere CDs einfach auf. Die vorhandene Sortierung bleibt erhalten.
-     * @param box - Gewählter CD-Ständer
+     *
+     * @param box - Index des Gewählten CD-Ständers
      */
     public void pack(int box){
         //TODO: 06 - Komprimieren eines CD-Ständers, von unten nach oben
@@ -75,6 +81,7 @@ public class CDCollectionHandler {
 
     /**
      * Diese Methode dient dazu, einen CD-Ständer zu sortieren nach Artist+Title. Gleichzeitig wird der CD-Ständer komprimiert.
+     *
      * @param box - Gewählter CD-Ständer
      */
     public void sort(int box){
